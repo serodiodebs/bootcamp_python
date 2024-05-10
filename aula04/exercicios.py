7d3f9cfb26d29db7c28c8031960334e2384ae0fe
serodiodebs <32377390+serodiodebs@users.noreply.github.com>
2024-05-10 17:48:52 -0300

    feat: adicionar exercicio 14 da aula 04

--- aula04/exercicios.py
+++ aula04/exercicios.py
@@ -134,18 +134,34 @@
 
 # 13. Filtragem de Dados em Dicionário
 # Dado um dicionário de estoque de produtos, filtrar aqueles com quantidade maior que 0.
-produtos = {"monitor":0, "teclado":500, "mouse":0, "cadeira":5000, "pad":90, "caixa de som":0}
+# produtos = {"monitor":0, "teclado":500, "mouse":0, "cadeira":5000, "pad":90, "caixa de som":0}
 
-for c,v in produtos.items():
-    if v > 0:
-        print(f'O valor do {c} é {v}')
+# for c,v in produtos.items():
+#     if v > 0:
+#         print(f'O valor do {c} é {v}')
 
-# com o list comprehension
-protudos_com_quantidade = [c for c,v in produtos.items() if v > 0]
-print(f'Os produtos com quantidades em estoque são: {protudos_com_quantidade}')
+# # com o list comprehension
+# protudos_com_quantidade = [c for c,v in produtos.items() if v > 0]
+# print(f'Os produtos com quantidades em estoque são: {protudos_com_quantidade}')
 
 # 14. Extração de Chaves e Valores
 # Dado um dicionário, criar listas separadas para suas chaves e valores.
+dic_qualquer:dict = {"laranja": 5, "banana": 3, "mamão": 12, "kiwi": 20}
+
+# Minha forma
+lista_chave:list = [fruta for fruta in dic_qualquer]
+lista_valor:list = [qtd for qtd in dic_qualquer.values()]
+
+print(f'Lista de chaves: {lista_chave}')
+print(f'Lista de valores: {lista_valor}')
+
+# Forma do prof.
+lista_chave2:list = list(dic_qualquer.keys())
+lista_valor2:list = list(dic_qualquer.values())
+
+print(f'Chaves: {lista_chave2}')
+print(f'Valores: {lista_valor2}')
+
 
 # 15. Contagem de Frequência de Itens
 # Dada uma string, contar a frequência de cada caractere usando um dicionário.
